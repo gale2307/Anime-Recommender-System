8dbcb2c00d5f6dd7e20ffc0bc70da6d96c3685a9
gale2307 <nicholaswilliam2307@gmail.com>
2021-05-12 15:13:07 -0700

    Now runs on streamlit

--- ncf.py
+++ ncf.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -16,28 +15,39 @@ class NCF(nn.Module):
     
     def __init__(self, num_users, num_items, ratings, all_animeIds):
         super().__init__()
-        #embedding of all users, each embedding having 8 dimensions
-        self.user_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=8)
-        #embedding of all animes, each embedding having 8 dimensions
-        self.item_embedding = nn.Embedding(num_embeddings=num_items, embedding_dim=8)
-        self.fc1 = nn.Linear(in_features=16, out_features=128)
-        self.fc2 = nn.Linear(in_features=128, out_features=32)
-        self.output = nn.Linear(in_features=32, out_features=1)
+        #mlp and mf embedding of all users, each embedding having 8 dimensions
+        self.user_mlp_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=8)
+        self.user_mf_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=8)
+        #mlp and mf embedding of all animes, each embedding having 8 dimensions
+        self.item_mlp_embedding = nn.Embedding(num_embeddings=num_items, embedding_dim=8)
+        self.item_mf_embedding = nn.Embedding(num_embeddings=num_items, embedding_dim=8)
+        self.fc1 = nn.Linear(in_features=16, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.output = nn.Linear(in_features=40, out_features=1)
         self.ratings = ratings
         self.all_animeIds = all_animeIds
         
     def forward(self, user_input, item_input):
         
         # Pass through embedding layers
-        user_embedded = self.user_embedding(user_input)
-        item_embedded = self.item_embedding(item_input)
+        user_mlp_embedded = self.user_mlp_embedding(user_input)
+        item_mlp_embedded = self.item_mlp_embedding(item_input)
+        
+        user_mf_embedded = self.user_mf_embedding(user_input)
+        item_mf_embedded = self.item_mf_embedding(item_input)
 
-        # Concat the two embedding layers
-        vector = torch.cat([user_embedded, item_embedded], dim=-1)
+        # Concat the two mlp embedding layers
+        mlp_vector = torch.cat([user_mlp_embedded, item_mlp_embedded], dim=-1)
+        
+        # Do element-wise multiplication on the two mf embedding layers
+        mf_vector = torch.mul(user_mf_embedded, item_mf_embedded)
 
         # Pass through dense layer
-        vector = nn.ReLU()(self.fc1(vector))
-        vector = nn.ReLU()(self.fc2(vector))
+        mlp_vector = nn.ReLU()(self.fc1(mlp_vector))
+        mlp_vector = nn.ReLU()(self.fc2(mlp_vector))
+        
+        # concat mlp and mf vectors
+        vector = torch.cat([mlp_vector, mf_vector], dim=-1)
 
         # Output layer
         pred = nn.Sigmoid()(self.output(vector))
@@ -72,15 +82,15 @@ def find_closest_user(user_anime, user_set_dict):
             closest_distance = distance
     return closest_user, closest_distance
 
-def print_anime(anime_list, anime_df):
+def print_anime(anime_list, anime_dict):
     #
     anime_name_list = list()
     for anime in anime_list:
-        anime_name_list.append(anime_df.loc[anime_df['MAL_ID'] == anime]['Name'].item())
+        anime_name_list.append(anime_dict[anime])
     print(anime_name_list)
     return(anime_name_list)
 
-def recommend_anime(model, user_id, all_animeIds, anime_df, user_watched = set(), num_recommend = 10):
+def recommend_anime(model, user_id, all_animeIds, anime_dict, user_watched = set(), num_recommend = 10):
     #model: NCF model used for recommendation
     #user_id: integer value representing a user
     #all_animeIds: set of all anime Ids
@@ -91,5 +101,5 @@ def recommend_anime(model, user_id, all_animeIds, anime_df, user_watched = set()
     predicted_labels = np.squeeze(model(torch.tensor([user_id]*len(non_interacted)), 
                                         torch.tensor(non_interacted)).detach().numpy())
     recommended_anime = [non_interacted[x] for x in np.argsort(predicted_labels)[::-1][0:num_recommend].tolist()]
-    recommended_anime = print_anime(recommended_anime, anime_df)
+    recommended_anime = print_anime(recommended_anime, anime_dict)
     return recommended_anime
--- webpage.py
+++ webpage.py
@@ -1,38 +1,45 @@
 import numpy as np
-import pandas as pd
 import pickle
 import streamlit as st
 import torch
 import ncf
 from ncf import NCF
 
+@st.cache
+def load_model(path):
+    model = NCF(353405, 48493, 0, 0)
+    model.load_state_dict(torch.load(path))
+    model.eval()
+    return model
+
+@st.cache(allow_output_mutation=True)
+def load_pickle(path):
+    with open(path, 'rb') as handle:
+        unpickled_file = pickle.load(handle)
+    return unpickled_file
+
 st.title('ARS (Anime Recommender System)')
 
-anime_df = pd.read_csv('anime.csv')
-
-model = NCF(353405, 48493, 0, 0)
-model.load_state_dict(torch.load("recommender_model_2.pt"))
-model.eval()
-
+#load NCF model
+model = load_model("recommender_model_3_3epochs.pt")
 
 #load user_set_dict from pickle file
-with open('user_set_dict.pickle', 'rb') as handle:
-    user_set_dict = pickle.load(handle)
+user_set_dict = load_pickle('user_set_dict.pickle')
 
 #load anime_dict from pickle file
-with open('anime_dict.pickle', 'rb') as handle:
-    anime_dict = pickle.load(handle)
+anime_dict = load_pickle('anime_dict.pickle')
 
+#drop down multi-select tool for users to input animes they watched
 anime_list = list(anime_dict.keys())
-
 user_list = st.multiselect('What Animes have you watched?', anime_list, format_func = anime_dict.get)
 
 'Your Anime list: ', user_list
 
+#slider to choose how many anime recommendations the model will output
 num_recommend = st.slider("Number of Anime Recommendations", 10, 100, 10)
 
-#add button to run model
+#button to run model
 if st.button("Give Anime Recommendation"):
     closest_user = ncf.find_closest_user(user_list, user_set_dict)
-    top_recommendations = ncf.recommend_anime(model, closest_user[0], anime_list, anime_df, user_list, num_recommend)
+    top_recommendations = ncf.recommend_anime(model, closest_user[0], anime_list, anime_dict, user_list, num_recommend)
     st.write(top_recommendations)
